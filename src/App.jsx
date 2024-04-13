8cd916c5606dd30c69461aff1afeec5bff7a6e4e
Enrique Jimenez <enriquejimenez468@gmail.com>
2024-04-13 19:49:11 -0400

    adding files to github

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css'
+
+
+import { styleReset } from 'react95';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import original from 'react95/dist/themes/original';
+
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
+
+function App() {
+
+  return (
+    <ThemeProvider theme={original}>
+      <GlobalStyles />
+        <nav className="topnav">
+        <ul>
+          <li>Home</li>
+          <li>Enrique's Page</li>
+          <li>Victoria's Page</li>
+        </ul>
+        </nav>
+      
+
+    </ThemeProvider>
+  )
+  
+}
+
+export default App
